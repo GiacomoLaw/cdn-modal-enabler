f3b5b70b35efe1b306d5c1267d30e5beb6375346
Giacomo Lawrance <giacomolawrance@gmail.com>
2019-06-07 12:54:06 +0100

    only work on single posts
    no longer applies site wide - only enabled on posts

--- tagdiv-cdn-modal.php
+++ tagdiv-cdn-modal.php
@@ -1,7 +1,8 @@
 <?php
 /**
  * Plugin Name: Newspaper tagDiv CDN modal image enabler
- * Description: Enable the modal image when using a CDN in the tagDiv Newspaper theme - switch off global modal image in settings.
+ * Plugin URI: https://giacomolaw.me
+ * Description: Enable the modal image when using a CDN - switch off global modal image in settings.
  * Author: Giacomo Lawrance
  * Author URI: https://giacomolaw.me
  * Version: 1.0
@@ -10,17 +11,22 @@
  */
 
 // td modal img on all images
-function add_responsive_class($content){
-	$content = mb_convert_encoding($content, 'HTML-ENTITIES', "UTF-8");
-	$document = new DOMDocument();
-	libxml_use_internal_errors(true);
-	$document->loadHTML(utf8_decode($content));
-	$imgs = $document->getElementsByTagName('img');
-	foreach ($imgs as $img) {
-		$existing_class = $img->getAttribute('class');
-		$img->setAttribute('class', "td-modal-image $existing_class");
-	}
-	$html = $document->saveHTML();
-	return $html;
+function tavdiv_modal(){
+    if(is_single()) {
+        add_filter('the_content', 'add_responsive_class');
+        function add_responsive_class($content){
+			$content = mb_convert_encoding($content, 'HTML-ENTITIES', "UTF-8");
+			$document = new DOMDocument();
+			libxml_use_internal_errors(true);
+			$document->loadHTML(utf8_decode($content));
+			$imgs = $document->getElementsByTagName('img');
+			foreach ($imgs as $img) {
+				$existing_class = $img->getAttribute('class');
+				$img->setAttribute('class', "td-modal-image $existing_class");
+			}
+			$html = $document->saveHTML();
+			return $html;
+		}
+    }
 }
-add_filter('the_content', 'add_responsive_class');
+add_action('template_redirect', 'tavdiv_modal');
